d6000a436b14c828961977791427dbc4e30d1e08
sarah-antillia <sarah@antillia.com>
2024-04-06 21:36:42 +0900

    Initial commit

---
+++ EBHIColorectalImageMaskDatasetGenerator.py
@@ -0,0 +1,237 @@
+# Copyright 2023-2024 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# Copyright 2023 antillia.com Toshiyuki Arai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# EBHIColorectalImageMaskDatasetGenerator.py
+# 2024/04/06 antillia.com
+
+import os
+import sys
+import glob
+import shutil
+import numpy as np
+
+import cv2
+from PIL import Image, ImageOps
+import traceback
+import random
+
+
+class EBHIColorectalImageMaskDatasetGenerator:
+  def __init__(self, resize=512, augmentation = False):
+    self.W      = resize
+    self.H      = resize
+    self.RESIZE = (self.W, self.H)
+
+    self.augmentation = augmentation
+    if self.augmentation:
+      self.hflip = True
+      self.vflip = True
+      self.rotation = True
+      self.ANGLES   =[90, 180, 270]
+
+  def generate(self, root_dir, output_dir):
+    dirs = os.listdir(root_dir)
+    print("--- dirs {}".format(dirs))
+    categories = []
+    for dir in dirs:
+      if os.path.isdir(root_dir + "/" + dir):
+        categories += [dir]
+    
+    random.seed(137)
+    # PIL color format (R, G, B)
+    mask_colors = [(110,  80,  50),   # mustard
+                   (240, 130, 240),   # violet
+                   (255, 255,   0),   # yellow
+                   (255, 255,  255),  # white
+                   (  0, 255,   0),   # green
+                   (  0,   0, 255)]   # blue
+
+    for i, category in enumerate(categories):
+      color = mask_colors[i]
+      full_category = os.path.join(root_dir, category)
+      print("--- category {}".format(full_category))
+
+      if not os.path.isdir(full_category):
+        print("NOT DIR skip {}".format(full_category))
+        continue
+      
+      images_dir  = os.path.join(full_category, "image")
+      masks_dir   = os.path.join(full_category, "label")
+
+      #output_category = os.path.join(output_dir, category)
+      output_images_dir = "/images"
+      output_masks_dir  = "/masks"
+      self.generate_one(images_dir, masks_dir, color, output_dir, category, 
+                output_images_dir, output_masks_dir)
+      
+  def generate_one(self, images_dir, masks_dir, color, output_dir, category, 
+                output_images_dir, output_masks_dir):
+    print("=== split_one {} {}".format(images_dir, output_dir))
+    #input("--- split_one")
+    image_files = glob.glob(images_dir + "/*.png")
+    random.shuffle(image_files)
+    num = len(image_files)
+    if num == 0:
+      input("FATAL Error")
+      return 
+    num_train = int(num * 0.7)
+    num_valid = int(num * 0.2)
+    num_test  = int(num * 0.1)
+    print("num_train {}".format(num_train))
+    print("num_valid {}".format(num_valid))
+    print("num_test  {}".format(num_test ))
+
+    train_files = image_files[:num_train]
+    valid_files = image_files[num_train:num_train+ num_valid]
+    test_files  = image_files[num_train+ num_valid:]
+    
+    category    = category.replace(" ", "_")
+    train_images_dir   = os.path.join(output_dir, "train/" + category + output_images_dir)
+    valid_images_dir   = os.path.join(output_dir, "valid/" + category + output_images_dir)
+    test_images_dir    = os.path.join(output_dir, "test/"  + category + output_images_dir)
+
+    train_masks_dir   = os.path.join(output_dir, "train/" + category + output_masks_dir)
+    valid_masks_dir   = os.path.join(output_dir, "valid/" + category + output_masks_dir)
+    test_masks_dir    = os.path.join(output_dir, "test/"  + category + output_masks_dir)
+    
+    self.resize_and_save(train_files, train_images_dir, color, train_masks_dir)
+    self.resize_and_save(valid_files, valid_images_dir, color, valid_masks_dir)
+    self.resize_and_save(test_files,  test_images_dir,  color, test_masks_dir )
+
+  def resize_and_save(self, image_files, dataset_images_dir, color, dataset_masks_dir):
+    if not os.path.exists(dataset_images_dir):
+      os.makedirs(dataset_images_dir)
+    print("=== resize_and_save  dataset_dir:{}".format(dataset_images_dir))
+    
+    if not os.path.exists(dataset_masks_dir):
+      os.makedirs(dataset_masks_dir)
+    print("=== resize_and_save  dataset_dir:{}".format(dataset_masks_dir))
+
+    for image_file in image_files:
+      basename = os.path.basename(image_file)
+
+      if os.path.exists(image_file):
+        image = Image.open(image_file)
+        image = image.resize(self.RESIZE)
+        basename = basename.replace(".png", ".jpg")
+        output_imagefile = os.path.join(dataset_images_dir, basename)
+        image.save(output_imagefile)
+        print("Saved {} to {}".format(image_file, output_imagefile))
+
+        if self.augmentation and dataset_images_dir.find("test") <0:
+          self.augment(image, basename, dataset_images_dir)
+
+        mask_file = image_file.replace("image", "label")
+        if not os.path.exists(mask_file):
+          print("=== FATAL Error not found mask file {}".format(mask_file))
+          break
+        mask = Image.open(mask_file)
+        mask = mask.resize(self.RESIZE)
+        mask = ImageOps.colorize(mask, black=(0, 0, 0), white=color)
+
+        basename = os.path.basename(mask_file)
+        basename = basename.replace(".png", ".jpg")
+        output_maskfile = os.path.join(dataset_masks_dir, basename)
+        mask.save(output_maskfile)
+        print("Saved {} to {}".format(mask_file, output_maskfile))
+
+        if self.augmentation and dataset_masks_dir.find("test") <0:
+          self.augment(mask, basename, dataset_masks_dir)
+
+      else:
+        if not os.path.exists(image_file):
+          print("NOT FOUND {}".format(image_file))
+
+
+  def pil2cv(self, image):
+    new_image = np.array(image, dtype=np.uint8)
+    if new_image.ndim == 2: 
+        pass
+    elif new_image.shape[2] == 3: 
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    elif new_image.shape[2] == 4: 
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+    return new_image
+
+  def augment(self, image, basename, output_dir):
+    image = self.pil2cv(image)
+    if self.hflip:
+      flipped = self.horizontal_flip(image)
+      output_filepath = os.path.join(output_dir, "hflipped_" + basename)
+      cv2.imwrite(output_filepath, flipped)
+      print("--- Saved {}".format(output_filepath))
+
+    if self.vflip:
+      flipped = self.vertical_flip(image)
+      output_filepath = os.path.join(output_dir, "vflipped_" + basename)
+      cv2.imwrite(output_filepath, flipped)
+      print("--- Saved {}".format(output_filepath))
+
+    if self.rotation:
+      self.rotate(image, basename, output_dir)
+
+  def horizontal_flip(self, image): 
+    print("shape image {}".format(image.shape))
+    if len(image.shape)==3:
+      return  image[:, ::-1, :]
+    else:
+      return  image[:, ::-1, ]
+
+  def vertical_flip(self, image):
+    if len(image.shape) == 3:
+      return image[::-1, :, :]
+    else:
+      return image[::-1, :, ]
+
+  def rotate(self, image, basename, output_dir):
+    for angle in self.ANGLES:      
+
+      center = (self.W/2, self.H/2)
+      rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=angle, scale=1)
+
+      rotated_image = cv2.warpAffine(src=image, M=rotate_matrix, dsize=(self.W, self.H))
+      output_filepath = os.path.join(output_dir, "rotated_" + str(angle) + "_" + basename)
+      cv2.imwrite(output_filepath, rotated_image)
+      print("--- Saved {}".format(output_filepath))
+     
+
+if __name__ == "__main__":
+  try:
+    root_dir   = "./EBHI-SEG/"
+    output_dir = "./EBHI-Colorectal-Cancer-ImageMask-Dataset-V2"
+    if os.path.exists(output_dir):
+      shutil.rmtree(output_dir)
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    resize       = 512
+    augmentation = True
+    generator = EBHIColorectalImageMaskDatasetGenerator(resize=resize, augmentation=augmentation)
+    generator.generate(root_dir, output_dir)
+
+  except:
+    traceback.print_exc()
--- README.md
+++ README.md
@@ -1,2 +1,143 @@
-# ImageMask-Dataset-EBHI-Colorectal-Cancer
-EBHI-SEG Pre-Augmented ImageMask Dataset 
+<h2> ImageMask-Dataset-EBHI-Colorectal-Cancer (2024/04/06)</h2>
+
+This is a Pre-Augmented EBHI-SEG ImageMask Dataset.<br>
+The dataset used here has been taken from the following figshare.com:EBHI-SEG<br>
+<pre>
+https://figshare.com/articles/dataset/EBHI-SEG/21540159/1
+</pre>
+
+You can download the <b>EBHI-Colorectal-Cancer-ImageMask-Dataset</b> from the google drive
+<a href="https://drive.google.com/file/d/1GM9leTP1XFW5qZuog14vdcqqpk_BUCfX/view?usp=sharing">
+EBHI-Colorectal-Cancer-ImageMask-Dataset-V2.zip
+</a>
+<br>
+
+<h3>1. Dataset Citation</h3>
+About dataset<br>
+<pre>
+This publicly available dataset contained 5,170 images of six types of tumor diﬀerentiation stages 
+and the corresponding ground truth images. 
+The dataset can provide researchers with new segmentation algorithms for medical diagnosis of colorectal 
+cancer, which can be used in the clinical setting to help doctors and patientsThe dataset in the present 
+study contained 5,710 histopathology images, including 2,855 histopathology section images and 2,855 
+ground truth images.
+</pre>
+
+Please see also:<br>
+<b>EBHI-Seg: A novel enteroscope biopsy histopathological hematoxylin and eosin image dataset for image segmentation tasks</b>
+<pre>
+https://www.frontiersin.org/articles/10.3389/fmed.2023.1114673/full
+</pre>
+<br>
+
+<h3>2. ImageMaskDataset Generation</h3>
+
+Please master dataset <b>EBHI-SEG</b> from the following link on figshare.com.<br> 
+<pre>
+https://figshare.com/articles/dataset/EBHI-SEG/21540159/1
+</pre>
+
+Please run the following Python script <a href="./EBHIColorectalImageMaskDatasetGenerator.py">EBHIColorectalImageMaskDatasetGenerator.py</a>.
+<br>
+<pre>
+python EBHIColorectalImageMaskDatasetGenerator.py
+</pre>
+, by which the following Augmented-ImageMask Dataset, <b>EBHI-Colorectal-Cancer-ImageMask-Dataset-V2</b>, from EBHI-SEG datatset will be created.<br>
+<pre>
+./EBHI-Colorectal-Cancer-ImageMask-Dataset-V2
+├─test
+│  ├─Adenocarcinoma
+│  │  ├─images
+│  │  └─masks
+│  ├─High-grade_IN
+│  │  ├─images
+│  │  └─masks
+│  ├─Low-grade_IN
+│  │  ├─images
+│  │  └─masks
+│  ├─Normal
+│  │  ├─images
+│  │  └─masks
+│  ├─Polyp
+│  │  ├─images
+│  │  └─masks
+│  └─Serrated_adenoma
+│      ├─images
+│      └─masks
+├─train
+│  ├─Adenocarcinoma
+│  │  ├─images
+│  │  └─masks
+│  ├─High-grade_IN
+│  │  ├─images
+│  │  └─masks
+│  ├─Low-grade_IN
+│  │  ├─images
+│  │  └─masks
+│  ├─Normal
+│  │  ├─images
+│  │  └─masks
+│  ├─Polyp
+│  │  ├─images
+│  │  └─masks
+│  └─Serrated_adenoma
+│      ├─images
+│      └─masks
+└─valid
+    ├─Adenocarcinoma
+    │  ├─images
+    │  └─masks
+    ├─High-grade_IN
+    │  ├─images
+    │  └─masks
+    ├─Low-grade_IN
+    │  ├─images
+    │  └─masks
+    ├─Normal
+    │  ├─images
+    │  └─masks
+    ├─Polyp
+    │  ├─images
+    │  └─masks
+    └─Serrated_adenoma
+        ├─images
+        └─masks
+</pre>
+To create our augmented dataset,we resized 
+all images and masks of the original dataset to be 512x512 pixel-size from 224x224, and 
+vertically, horizontally flipped, and futhermore rotated by angle in [90, 180, 270].
+For simplicity, no image deformation or distortion operation applied.<br>
+In the augmented dataset, we also colorized the mask images per category with different colors 
+instead of the original black and white color.<br>
+ 
+
+<br>
+Generated Train Dataset Samples<br>
+<hr>
+Adenocarcinoma<br>
+<img src="./asset/Adenocarcinoma_images.png" width="1024" height="auto"><br>
+<img src="./asset/Adenocarcinoma_masks.png" width="1024" height="auto"><br>
+<hr>
+High-grade_IN<br>
+<img src="./asset/High-grade_IN_images.png" width="1024" height="auto"><br>
+<img src="./asset/High-grade_IN_masks.png" width="1024" height="auto"><br>
+<hr>
+Low-grade_IN<br>
+<img src="./asset/Low-grade_IN_images.png" width="1024" height="auto"><br>
+<img src="./asset/Low-grade_IN_masks.png" width="1024" height="auto"><br>
+<hr>
+  
+Normal<br>
+<img src="./asset/Normal_images.png" width="1024" height="auto"><br>
+<img src="./asset/Normal_masks.png" width="1024" height="auto"><br>
+<hr>
+
+Polyp<br>
+<img src="./asset/Polyp_images.png" width="1024" height="auto"><br>
+<img src="./asset/Polyp_masks.png" width="1024" height="auto"><br>
+<hr>
+
+Serrated_adnoma<br>
+<img src="./asset/Serrated_adnoma_images.png" width="1024" height="auto"><br>
+<img src="./asset/Serrated_adnoma_masks.png" width="1024" height="auto"><br>
+<hr>
